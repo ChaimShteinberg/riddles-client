5a6ba4691794e6dafadfc37a5344361a2e5a68de
Chaim Shteinberg <a0533197133@gmail.com>
2025-07-20 11:11:34 +0300

    create player

--- client/players.client.js
+++ client/players.client.js
@@ -1,16 +1,21 @@
-import { getAllPlayersApi } from '../api/players.api.js'
+import { addPlayerApi, getAllPlayersApi } from '../api/players.api.js'
 
-export async function playerService(username){
+export async function playerService(username) {
     const platers = await getPlayers();
     for (const player of platers) {
         if (player.username === username)
-            return player
-    }
+            return player;
+    };
+    await createPlayer(username);
 }
 
 export async function getPlayers() {
     const players = await getAllPlayersApi();
     return players;
+}
+
+export async function createPlayer(username) {
+    await addPlayerApi({"username": username});
 }
 
 console.log(await playerService("chaim"))
